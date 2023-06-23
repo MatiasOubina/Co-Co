d26c18da94ca417cf59c5c94c9930c26b5c840f8
Matías Oubiña <81721059+MatiasOubina@users.noreply.github.com>
2023-06-22 21:55:59 -0300

    Add files via upload

--- mail.php
+++ mail.php
@@ -5,25 +5,18 @@ $empresa = $_POST['empresa'];
 $correo = $_POST['correo'];
 $cargo = $_POST['cargo'];
 $mensaje = $_POST['mensaje'];
-$newsletter = $_POST['newsletter']
+$newsletter = $_POST['newsletter'];
 
-$body = <<<HTML 
-        <h1>Contacto desde la web</h1>
-        <p>De: $nombre</p>
-        <p>Empresa: $empresa</p>
-        <p>Correo: $correo</p>
-        <p>Cargo: $cargo</p>>
-        <h2>Mensaje: $mensaje</h2>
-        HTML;
-
-$headers = "MIME-Version: 1.0 \r\n";
-$headers. = "Content-type: text/html; charset=utf-8 \r\n";
-$headers. = "From: $nombre <$correo>";
-$headers. = "To: Sitio web <avqstest@gmail.com> \r\n";
+$mensaje = "Mensaje enviado por" . $nombre . ",\r\n";
+$mensaje .= "Correo: " . $correo . " \r\n";
+$mensaje .= "Empresa: " . $empresa . " \r\n";
+$mensaje .= "Cargo: " . $cargo . " \r\n";
+$mensaje .= "Mensaje: " . $mensaje . " \r\n"; 
 
 $para = 'avqstest@gmail.com';
+$asunto = 'Este mail es enviado desde Co Co';
 
-mail($para, 'Mensaje desde la web' $body)
+mail($para, $asunto, utf8_decode($mensaje), $header);
 
 header('Location:gracias.html');
 
